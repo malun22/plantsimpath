6614ed34280f02f41bc76668937026f7ce31885a
Luca Bernstiel <luca.bernstiel@iml.fraunhofer.de>
2026-05-26 11:46:12 +0200

    Bump version: 0.0.5 → 0.0.6

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "plantsimpath"
 copyright = "2025, Luca Bernstiel"
 author = "Luca Bernstiel"
-release = "0.0.5"
+release = "0.0.6"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
